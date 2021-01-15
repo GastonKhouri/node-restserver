29607306f32296755e6be3dbf0c58eb5ffdf1c34
GastonKhouri <gastonlozano2016@yahoo.com>
2021-01-15 00:07:02 -0430

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://gaston:bK7eye1FhxWEGbmy@cluster0.vdajt.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
