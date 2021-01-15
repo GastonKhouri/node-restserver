b41012602046e1c8455fb5da86b51f4f00b7dd78
GastonKhouri <gastonlozano2016@yahoo.com>
2021-01-14 21:34:50 -0430

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,25 @@
  * ==========================
  **/
 process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * ========================== 
+ *  Entorno
+ * ==========================
+ **/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/**
+ * ========================== 
+ *  Bases de datos
+ * ==========================
+ **/
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://gaston:bK7eye1FhxWEGbmy@cluster0.vdajt.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
